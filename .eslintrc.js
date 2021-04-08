f265db90c256dadd4861835512de1904c2c48f05
nicotorresel <nicotorresel@gmail.com>
2021-04-08 12:17:44 -0300

    asda

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,62 +1,65 @@
 module.exports = {
   env: {
-    browser: false,
+    browser: true,
     es2021: true,
-    node: true
   },
   extends: [
-    'plugin:react/recommended',
-    'plugin:prettier/recommended',
-    'plugin:react-hooks/recommended'
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+    "plugin:react-hooks/recommended",
   ],
-  plugins: ['react', 'prettier', 'import'],
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: 'module'
+    sourceType: "module",
   },
+  plugins: ["react", "prettier", "import"],
   rules: {
-    'no-console': 'warn',
-    'react/prop-types': 'off',
-    'prettier/prettier': [
-      'error',
+    "no-console": "warn",
+    "react/prop-types": "off",
+    "prettier/prettier": [
+      "error",
       {
         printWidth: 100,
-        trailingComma: 'all',
+        trailingComma: "all",
         tabWidth: 2,
         semi: true,
         singleQuote: false,
-        bracketSpacing: false,
-        arrowParens: 'always',
-        endOfLine: 'auto'
-      }
+        bracketSpacing: true,
+        arrowParens: "always",
+        endOfLine: "auto",
+      },
     ],
-    'no-unused-vars': [
-      'warn',
+    "no-unused-vars": [
+      "warn",
       {
-        args: 'after-used',
+        args: "after-used",
         ignoreRestSiblings: false,
-        argsIgnorePattern: '^_.*?$'
-      }
+        argsIgnorePattern: "^_.*?$",
+      },
     ],
-    'import/order': ['warn', { 'newlines-between': 'always' }],
-    'react/self-closing-comp': 'warn',
-    'react/jsx-sort-props': [
-      'warn',
+    "import/order": ["warn", { "newlines-between": "always" }],
+    "react/self-closing-comp": "warn",
+    "react/jsx-sort-props": [
+      "warn",
       {
         callbacksLast: true,
         shorthandFirst: true,
         noSortAlphabetically: false,
-        reservedFirst: true
-      }
+        reservedFirst: true,
+      },
     ],
-    'padding-line-between-statements': [
-      'error',
-      { blankLine: 'always', prev: '*', next: 'return' },
-      { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
-      { blankLine: 'any', prev: ['const', 'let', 'var'], next: ['const', 'let', 'var'] }
-    ]
-  }
+    "padding-line-between-statements": [
+      "error",
+      { blankLine: "always", prev: "*", next: "return" },
+      { blankLine: "always", prev: ["const", "let", "var"], next: "*" },
+      {
+        blankLine: "any",
+        prev: ["const", "let", "var"],
+        next: ["const", "let", "var"],
+      },
+    ],
+  },
 };
